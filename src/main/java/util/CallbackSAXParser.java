18f61b58360e1d2ddbea570a1a55350a4c1368c4
almudenaris <almudenaris@gmail.com>
2014-10-21 14:59:23 +0200

    write in rdf/ttl the entire model transform the annotations to SDO

--- src/main/java/util/CallbackSAXParser.java
+++ src/main/java/util/CallbackSAXParser.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Stack;
@@ -16,12 +17,14 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.DCTerms;
 import com.hp.hpl.jena.vocabulary.VCARD;
 
 
 public class CallbackSAXParser extends DefaultHandler {
-	
+
 	/**
 	 * Interfaz para implementar una pila de callbacks en SAX
 	 * Extraido de http://www.gamasutra.com/view/feature/2678/efficient_xml_file_reading_for_.php
@@ -30,99 +33,161 @@ public class CallbackSAXParser extends DefaultHandler {
 	 */	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	
-	
+
+
 	private abstract class Receiver {		
-		
+
 		abstract Receiver processData(String name, Attributes attrs);
 
 		abstract void finishProcessData(String name);
 	}
-	
+
 	// TODO Receiver para SDO
 	private class SDOReceiver extends Receiver{
 
 		@Override
 		Receiver processData(String name, Attributes attrs) {
-			// TODO averiguar qué concepto es....¿es necesario? ¿o simplemente añadimos statement?
-			logger.info("Estamos en una nueva sentence anotada");
-			logger.info(this.toString()+" name: "+name+" attributes: "+attrs.toString());
+//			logger.info("Estamos en una nueva sentence anotada");
+//			logger.info(this.toString()+" name: "+name+" attributes: "+attrs.toString());
 			return new SDOReceiver();
 		}
 
 		@Override
 		void finishProcessData(String name) {
-			// TODO Auto-generated method stub
-			
+			// hacemos el procesamiento al final ya que será cuando tengamos el contenido de la etiqueta
+			String label = null;
+			//FIXME esto no se corresponde con SDO por las anotaciones que nos han pasado
+			if (name.equals(DRI_Approach)){
+				label = "Motivation";
+			}
+			else if (name.equals(DRI_Background)){
+				label = "Background";
+			}
+			else if (name.equals(DRI_Challenge)){
+				label = "Contribution";
+			}
+			else if (name.equals(DRI_Challenge_Goal)){
+				label = "Goal";
+			}
+			else if (name.equals(DRI_Challenge_Hypothesis)){
+				label = "Hypothesis";
+			}
+			else if (name.equals(DRI_FutureWork)){
+				label = "FutureWork";	
+			}
+			else if (name.equals(DRI_Outcome)){
+				label = "Motivation";
+			}
+			else if (name.equals(DRI_Outcome_Contribution)){
+				label = "Contribution";
+			}
+			else if (name.equals(DRI_Unspecified)){
+				label = "Thing";
+			}			
+			else 
+			{
+				label = "Thing";
+			}
+			Property sdoP = model.createProperty(sdo+label);			
+			// create the resource
+			Resource sentence = model.createResource(":sentence"+contador);			
+			sentence.addProperty(sdoP, contenido);
+			contador++;
+			contenido = new String();
 		}
-		
+
 	}
-	
+
 	// Receiver del principio del documento
 	// mi objetivo es construir un modelo RDF
 	private class StartReceiver extends Receiver{
 
 		public void finishProcessData(String name) {
-			
+
 		}
-	
+
 		public Receiver processData(String name, Attributes attrs) {			
-			// create an empty Model
-			model = ModelFactory.createDefaultModel();
 
 			// TODO cogemos el nombre del documento
 			logger.info("Empezamos a procesar el documento");
-			
+
 			logger.info(this.toString()+" name: "+name+" attributes: "+attrs.toString());
 			for (int i = 0; i< attrs.getLength(); i++){
 				logger.info("Atributo: "+attrs.getValue(i));
 			}
 			String nameFile = attrs.getValue("name");
 			logger.info("Nombre del documento: "+nameFile);
-			Resource fileResource = model.createResource(nameFile);
-			fileResource.addProperty(VCARD.FN, "");
-			model.write(System.out);
+
+			// create an empty Model
+			model = ModelFactory.createDefaultModel();
+
+			a = model.createProperty(rdf+"type");
+			model.setNsPrefix("rdf", rdf);
+
+			biboArticle = model.createProperty(bibo+"Article");
+			model.setNsPrefix("bibo", bibo);
+
+			Property sdoDiscourse = model.createProperty(sdo+"ScientificDiscourse");
+			model.setNsPrefix("sdo", sdo);
+
+			// create the paper
+			Resource fileResource = model.createResource(":paper");			
+			fileResource.addProperty(a, biboArticle);
+			fileResource.addProperty(a, sdoDiscourse);	
+
+
+			//			model.write(System.out,"RDF/XML-ABBREV");
+			model.write(System.out, "TURTLE");
+
 			return new ArticleReceiver();
 		}		
 	}	
-	
-	
+
+
 	private class ArticleReceiver extends Receiver {
 
 		@Override
 		Receiver processData(String name, Attributes attrs) {
-			// TODO aqui podemos coger el nombre bonito del título del articulo
+			// al comienzo no es necesario hacer nada
 			logger.info("Estamos en article receiver");
-			logger.info(this.toString()+" name: "+name+" attributes: "+attrs.toString());
+			logger.info(this.toString()+" name: "+name+" attributes: "+attrs.toString());			
 			return null;
 		}
 
 		@Override
 		void finishProcessData(String name) {
+			// create the title
+			String nameFile = contenido;
+			Resource titleResource = model.createResource(":title");			
+			Property docoTitle = model.createProperty(doco+"Title");
+			model.setNsPrefix("doco", doco);
+			titleResource.addProperty(a, docoTitle);
+			titleResource.addProperty(DCTerms.description,nameFile);
+			model.write(System.out, "TURTLE");
 			// añadimos a la pila un SDO
 			parserStack.push(new SDOReceiver());
 		}
-		
+
 	}	
 
-	
+
 	/**
-	* Atributos
-	*/
+	 * Atributos
+	 */
 	private javax.xml.parsers.SAXParser saxParser;
 	private String xmlFilename;	
 	private Stack<Receiver> parserStack;
 	private Model model;
 
 	public CallbackSAXParser() {
-		  parserStack = new Stack<Receiver>();
+		parserStack = new Stack<Receiver>();
 	}
-	
+
 	public void startElement(String uri, String local, String name, Attributes attrs)  throws SAXException {
-    	// el elemento que está en la cima de la pila
+		// el elemento que está en la cima de la pila
 		Receiver candidate = parserStack.peek();
 		Receiver followUp = null;	
-		
+
 		if (name.equals(title))
 			followUp = ((ArticleReceiver)candidate).processData(name, attrs);
 		else if (name.equals(DRI_Approach) || name.equals(DRI_Background) || name.equals(DRI_Challenge) ||
@@ -132,49 +197,50 @@ public class CallbackSAXParser extends DefaultHandler {
 			followUp = ((SDOReceiver)candidate).processData(name, attrs);		
 		else 
 			followUp = (candidate).processData(name, attrs);
-		
+
 		// el elemento siguiente		
-    	if (followUp!=null) {
-    		parserStack.push(followUp);
-    	}
-    	else
-    		parserStack.push(candidate);    	
-    }
-    
-    public void endElement(String uri, String local, String name) throws SAXException {
-    	logger.info("EStamos en end elemento: "+uri+" - "+local+" - "+name);
-    	Receiver top = parserStack.pop();
-    	top.finishProcessData(name);
-    } 
-    
-    /* 
+		if (followUp!=null) {
+			parserStack.push(followUp);
+		}
+		else
+			parserStack.push(candidate);    	
+	}
+
+	public void endElement(String uri, String local, String name) throws SAXException {
+		logger.info("EStamos en end elemento: "+uri+" - "+local+" - "+name);		
+		Receiver top = parserStack.pop();
+		top.finishProcessData(name);
+	} 
+
+	/* 
 	 * Esta funcion es llamada cuando ve el contenido de una etiqueta      
 	 */
-    public void characters(char buf[], int offset, int len) throws SAXException{
-    	String contenido = new String(buf, offset, len);
-    	System.out.println(contenido);
-    }
-    
+	public void characters(char buf[], int offset, int len) throws SAXException{
+		// FIXME no coge bien todo el contenido entre etiquetas
+		contenido = contenido+new String(buf, offset, len);
+		logger.debug("contenido de la etiqueta "+contenido);
+	}
+
 	public boolean init(String xmlFile){
 		boolean _ok = false;
 		// Crear la fabrica utilizar para SAX 
 		SAXParserFactory factory  = SAXParserFactory.newInstance();
-		
+
 		try { 
-		    saxParser = factory.newSAXParser();
-		    xmlFilename = xmlFile;
-		    parserStack.clear();
-		    // Todas las inicializaciones adicionales que hace nuestro parser
-		    _ok = true;
+			saxParser = factory.newSAXParser();
+			xmlFilename = xmlFile;
+			parserStack.clear();
+			// Todas las inicializaciones adicionales que hace nuestro parser
+			_ok = true;
 
 		} catch (ParserConfigurationException pexc) { 
-		    pexc.printStackTrace();
+			pexc.printStackTrace();
 		} catch (SAXException saxex) { 
-		    saxex.printStackTrace();
+			saxex.printStackTrace();
 		} 
 		return _ok;
 	}
-	
+
 	public void parse() {
 		try {
 			// StartReceiver implementa Receiver y es el responsable de 
@@ -183,19 +249,19 @@ public class CallbackSAXParser extends DefaultHandler {
 			InputStream ficEntrada=null;
 			if ((ficEntrada=new FileInputStream(xmlFilename)) != null)
 				saxParser.parse( ficEntrada, this );
-			
+
 		} catch (SAXException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void end() {
 		// Si hay que hacer algo al finalizar
 	}
-	
-	
+
+
 	// EJEMPLO DE USO
 	public static void main(String[] args) {
 		CallbackSAXParser p = new CallbackSAXParser();
@@ -203,8 +269,25 @@ public class CallbackSAXParser extends DefaultHandler {
 			p.parse();
 		}
 		p.end();
+		FileWriter out = null;
+		try {
+		   // OR Turtle format - compact and more readable
+		  // use this variant if you're not sure which to use!
+		  out = new FileWriter( "mymodel.ttl" );
+		  p.model.write( out, "Turtle" );
+			p.logger.debug("total number of sentences: "+p.contador);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+		  if (out != null) {
+		    try {out.close();} catch (IOException ignore) {}
+		  }
+		}
+
 	}
-	
+
 	// fields for SDO annotation
 	private final String DRI_Background ="DRI_Background";
 	private final String DRI_Challenge ="DRI_Challenge";
@@ -217,6 +300,17 @@ public class CallbackSAXParser extends DefaultHandler {
 	private final String DRI_Challenge_Hypothesis ="DRI_Challenge_Hypothesis";
 	private final String Sentence ="Sentence";
 	private final String title = "article-title";
-//	private final String document ="Document";
+	//	private final String document ="Document";
+
+	// prefix for ontology
+	private final String doco = "http://purl.org/spar/doco/";
+	private final String deo = "http://purl.org/spar/deo/";
+	private final String bibo = "http://purl.org/ontology/bibo/";
+	private final String sdo = "http://purl.org/drinventor/sci-doc";
+	private String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	protected Property a;
+	protected Property biboArticle;
+	private String contenido;	
+	private int contador = 0;
 
 }
