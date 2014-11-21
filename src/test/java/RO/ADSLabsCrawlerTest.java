28022bdae3d4617fa09d928c118d4c5f13c682a2
almudenaris <almudenaris@gmail.com>
2014-11-21 11:50:02 +0100

    test class for  adslabs

---
+++ src/test/java/RO/ADSLabsCrawlerTest.java
@@ -0,0 +1,50 @@
+package RO;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.oeg.ro.ADSLabsCrawler;
+import es.oeg.ro.transfer.ADSLabsResultsBean;
+
+public class ADSLabsCrawlerTest {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	ADSLabsCrawler crawler = new ADSLabsCrawler();
+
+	@Test
+	public void executeQueryWithTerms() throws IOException, URISyntaxException{
+		//		# Simple search for "black holes", restricted to astronomy content
+		//		http://adslabs.org/adsabs/api/search/?q=black+holes&filter=database:astronomy&dev_key=abc123
+		ADSLabsResultsBean beanResults = crawler.search("black holes");
+		assertNotNull(beanResults);
+		assertNotNull(beanResults.getMeta());
+		assertNotNull(beanResults.getResults());
+		assertTrue(beanResults.getResults().get_docs().size() > 0);		
+	}
+	
+	@Test
+	public void executeQueryWithFilter() throws ClientProtocolException, IOException, URISyntaxException{
+		ADSLabsResultsBean beanResults = crawler.search("transiting exoplanets", "database:astronomy");
+		assertNotNull(beanResults);
+		assertNotNull(beanResults.getMeta());
+		assertNotNull(beanResults.getResults());
+		assertTrue(beanResults.getResults().get_docs().size() > 0);		
+	}
+	
+	@Test
+	public void executeQueryWithAuthorFilter() throws IOException, URISyntaxException{
+		ADSLabsResultsBean beanResults = crawler.search("author:Accomazzi, Alberto");		
+		assertNotNull(beanResults);
+		assertNotNull(beanResults.getMeta());
+		assertNotNull(beanResults.getResults());
+		assertTrue(beanResults.getResults().get_docs().size() > 0);		
+	}
+}
