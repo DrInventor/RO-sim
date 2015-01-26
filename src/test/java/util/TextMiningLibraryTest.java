1dbf68dcef90408b9a3d8328f538219f22588f49
almudenaris <almudenaris@gmail.com>
2015-01-26 17:07:44 +0100

    added authors extraction test and graph extraction

--- src/test/java/util/TextMiningLibraryTest.java
+++ src/test/java/util/TextMiningLibraryTest.java
@@ -69,74 +69,113 @@ public class TextMiningLibraryTest {
 	
 	
 	@Test
-	public void testExtractSentences() throws DRIexception{
+	public void testExtractSentencesPDF() throws DRIexception{
 
 		// Process a PDF file stored at the path
-//		Document doc;
-		Document docLoaded;
+		Document doc;
 		
-//		doc = Factory.getPDFimporter().parsePDF(PDF_FILE_PATH);
-//		assertNotNull(doc);
-		
-		docLoaded = Factory.createNewDocument(SERIALIZED_FILE_PATH);
-		assertNotNull(docLoaded);
+		doc = Factory.getPDFimporter().parsePDF(PDF_FILE_PATH);
+		assertNotNull(doc);
+//		
 
 		// Geth the sentence number 50 in the document
-//		Sentence sentDoc10 = doc.extractSentences(SentenceSelectorENUM.ALL).get(10);
-//		assertNotNull(sentDoc10);
-		Sentence sentDoc10Serialized = docLoaded.extractSentences(SentenceSelectorENUM.ALL).get(1);
+		Sentence sentDoc10 = doc.extractSentences(SentenceSelectorENUM.ALL).get(10);
+		assertNotNull(sentDoc10);
+		logger.info(sentDoc10.getSectionName());
+		logger.info(sentDoc10.getText());
+		//TODO  INFO [main] (TextMiningLibraryTest.java:86) - IMPLEMENTED_IN_FUTURE_VERSIONS_OF_THE_LIB
+		// function not implemented yet
+		logger.info(sentDoc10.getRhethoricalClass().name());
+		
+	}
+	
+	@Test
+	public void testExtractSentencesSerializedDocument() throws DRIexception{
+		
+		// Process a PDF file stored at the path
+		Document docLoaded;
+		
+	
+		docLoaded = Factory.createNewDocument(SERIALIZED_FILE_PATH);
+		assertNotNull(docLoaded);
+		
+		assertNotNull(docLoaded.getXMLString());
+		logger.info(docLoaded.getXMLString());
+		
+		// Geth the sentence number 50 in the document		
+		Sentence sentDoc10Serialized = docLoaded.extractSentences(SentenceSelectorENUM.ALL).get(50);
 		assertNotNull(sentDoc10Serialized);		
 	}
 	
+	
 	@Test
-	public void testExtractGraph() throws DRIexception{
+	public void testExtractGraphFromPDF() throws DRIexception{
 		// Process a PDF file stored at the path
 		Document doc;
-		Document docLoaded;
 		
-//		doc = Factory.getPDFimporter().parsePDF(PDF_FILE_PATH);
-//		assertNotNull(doc);
+		doc = Factory.getPDFimporter().parsePDF(PDF_FILE_PATH);
+		assertNotNull(doc);
+		// Geth the sentence number 10 in the document
+		Sentence sentDoc10 = doc.extractSentences(SentenceSelectorENUM.ALL).get(10);
+		assertNotNull(sentDoc10);
 		
-		docLoaded = Factory.createNewDocument(SERIALIZED_FILE_PATH);
-		assertNotNull(docLoaded);
-
-		// Geth the sentence number 50 in the document
-//		Sentence sentDoc10 = doc.extractSentences(SentenceSelectorENUM.ALL).get(10);
-//		assertNotNull(sentDoc10);
-		Sentence sentDoc10Serialized = docLoaded.extractSentences(SentenceSelectorENUM.ALL).get(1);
-		assertNotNull(sentDoc10Serialized);
+		// Get the graph of this sentence
+		Graph grph = doc.extractSentenceGraph(sentDoc10.getId(), GraphTypeENUM.COMPACT);
+		assertNotNull(grph);
 		
-		
-		// Get the COMPACT graph generated from the first sentence of the document (sentenceList)
-		Graph grphSerialized = docLoaded.extractSentenceGraph(sentDoc10Serialized.getId(), GraphTypeENUM.COMPACT);
-		assertNotNull(grphSerialized);
-//		
-//		Graph grph = doc.extractSentenceGraph(sentDoc10Serialized.getId(), GraphTypeENUM.COMPACT);
-//		assertNotNull(grph);
-
 		// Print the compact graph as a TREE
-		logger.info(grphSerialized.graph_AsString(GraphToStringENUM.TREE));
+		logger.info(grph.graph_AsString(GraphToStringENUM.TREE));
 	}
 	
 	@Test
-	public void testExtractSummary() throws DRIexception{
-		Document doc;
+	public void testExtractGraphFromXML() throws DRIexception{
 		Document docLoaded;
+		docLoaded = Factory.createNewDocument(SERIALIZED_FILE_PATH);
+		assertNotNull(docLoaded);
+		Sentence sentDoc10Serialized = docLoaded.extractSentences(SentenceSelectorENUM.ALL).get(1);
+		assertNotNull(sentDoc10Serialized);
+		// Get the COMPACT graph generated from the first sentence of the document (sentenceList)
+		Graph grphSerialized = docLoaded.extractSentenceGraph(sentDoc10Serialized.getId(), GraphTypeENUM.COMPACT);
+		assertNotNull(grphSerialized);
+	}
+	
+	@Test
+	public void testExtractSummaryFromPDF() throws DRIexception{
+		Document doc;
 		
-//		doc = Factory.getPDFimporter().parsePDF(PDF_FILE_PATH);
-//		assertNotNull(doc);
+		doc = Factory.getPDFimporter().parsePDF(PDF_FILE_PATH);
+		assertNotNull(doc);
+		List<Sentence> summarySentences = doc.extractSummary(10, SummaryTypeENUM.TOP_TFIDF);
+		assertNotNull(summarySentences);
+		assertTrue(summarySentences.size() > 0);
 		
+		for(Sentence s: summarySentences)
+			logger.info(s.getText());
+
+	}
+	
+	@Test
+	public void testExtractSummaryFromXML() throws DRIexception{
+		Document docLoaded;
+		//		
 		docLoaded = Factory.createNewDocument(SERIALIZED_FILE_PATH);
 		assertNotNull(docLoaded);
 		
 		// Get the extractive summary made of the top 10 TFIDF scored sentences.
 		List<Sentence> summarySentencesSerialized = docLoaded.extractSummary(5, SummaryTypeENUM.TOP_TFIDF);
 		assertNotNull(summarySentencesSerialized);
-		assertTrue(summarySentencesSerialized.size() > 0);
-		
-//		List<Sentence> summarySentences = doc.extractSummary(10, SummaryTypeENUM.TOP_TFIDF);
-//		assertNotNull(summarySentences);
-//		assertTrue(summarySentences.size() > 0);
-		
+		assertTrue(summarySentencesSerialized.size() > 0);		
 	}
+	
+	@Test
+	public void testExtractAuthorsFromPDF() throws DRIexception{
+		Document doc;
+		
+		doc = Factory.getPDFimporter().parsePDF(PDF_FILE_PATH);
+		String contributors = doc.extractContributors(); 
+		assertNotNull(contributors);
+		
+		logger.info(contributors);
+	}
+	
 }
